e4c8cc82f929ebc8bc8287e1823b452362906903
Larissa <lari_conde94@hotmail.com>
2017-07-28 14:42:50 -0300

    Right triangle exercise

---
+++ src/introdutory/triangle/RightTriangle.java
@@ -0,0 +1,21 @@
+package introdutory.triangle;
+
+import java.util.Scanner;
+
+public class RightTriangle {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n;
+
+        System.out.print("Enter the length of the triangle's base: ");
+        n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
