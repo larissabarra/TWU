7b6557fd8c131325905491c3a6c9296eec7e5967
Larissa <lari_conde94@hotmail.com>
2017-07-28 15:12:31 -0300

    Diamond exercise

---
+++ src/introdutory/diamond/Diamond.java
@@ -0,0 +1,34 @@
+package introdutory.diamond;
+
+import java.util.Scanner;
+
+public class Diamond {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n;
+
+        System.out.print("Enter n: ");
+        n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < (n - i); j++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k < 2*i-1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k < (2 * (n - i) - 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
