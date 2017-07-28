2b439f2249cf0ff33e061b990e7437d19d27136f
Larissa <lari_conde94@hotmail.com>
2017-07-28 15:23:35 -0300

    FizzBuzz game

---
+++ src/introdutory/logic/FizzBuzz.java
@@ -0,0 +1,24 @@
+package introdutory.logic;
+
+public class FizzBuzz {
+
+    public static void main(String[] args) {
+        fizzbuzz (100);
+    }
+
+    public static void fizzbuzz (int n) {
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0) {
+                if (i % 5 == 0) {
+                    System.out.println("FizzBuzz");
+                } else {
+                    System.out.println("Fizz");
+                }
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+}
